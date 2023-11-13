cb5f90bfc46a65c1903ad766b63da55159e638c4
LeoUpperThrower4 <leonardosilva0411@outlook.com>
2023-11-13 18:40:29 -0300

    create multiple orders

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,54 +9,44 @@ import api from '../../utils/api';
 
 
 export function Cart() {
-const { addToast } = useToast();
-const { cart, setCart } = useCart();
-const client_id = JSON.parse(localStorage.getItem("@taNaMesa:user")).id;
+  const { addToast } = useToast();
+  const { cart, setCart } = useCart();
+  const client_id = JSON.parse(localStorage.getItem("@taNaMesa:user")).id;
 
-const finishOrder = async () =>{
-  let total = 0
-
-  let order = {
-    "clientId": client_id,
-    "id": 11,  
-    "products": cart.map(item => {return { productId : item.id , quantity : item.quantity, price: item.price, observation: item.observation ?? null }}),
-  }
-
-
+  async function finishOrder() {
+    let order = {
+      "clientId": client_id,
+      "products": cart.map(item => {return { productId : item.id , quantity : item.quantity, price: item.price, observation: item.observation ?? null }}),
+    }
     try {   
       const response = await api.post('/order', order);
-      addToast({ type: "success", title: "Pedido", message:"Realizado com Sucesso" });
-
+      if (response.status === 201) {
+        addToast({ type: "success", title: "Pedido", message:"Realizado com Sucesso" });
+      } else {
+        addToast({ type: "error", title: "Erro ao realizar pedido", message: "Tente novamente" });
+      }
+    }
+    catch (err) {
+      addToast({ type: "error", title: "Erro ao realizar pedido", message: handleError(err) });
+    }
+    setCart([])
   }
- catch (err) {
-  const message = handleError(err);
-  const title = 'Erro ao realizar pedido'
-  addToast({ type: "error", title, message });
-}
-setCart([])
-}
-
-
-
-
   return(
-    <div className={cart.length > 0? `${styles.cart} ${styles.cartShow}` : `${styles.cart}`}>
+    <div 
+      className={cart.length > 0? `${styles.cart} ${styles.cartShow}` : `${styles.cart}`}
+    >
       <div className={styles.orderTitle}>
       <h3 >Order#{client_id}</h3>
       </div>
       <div className={styles.requests}>
       {cart.map(product => {
-      return(   
-        <CartCard key={product.id} product={product} />
-        
-      )})}
-      
-      
+        return(   
+          <CartCard key={product.id} product={product} />
+        )})}
       </div>
       <div className={styles.submit}>
         <button className={styles.submitButton} onClick={finishOrder}>Fechar Carrinho</button>
       </div>
-      </div>
-
-)
+    </div>
+  )
 }
